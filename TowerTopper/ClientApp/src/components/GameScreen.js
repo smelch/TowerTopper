3fd76799b8b161689969a0de663810414320b279
Eric Blatz <blatz.eric@gmail.com>
2020-08-26 04:10:20 -0700

    Split ImageLoader from GameScreen

--- TowerTopper/ClientApp/src/components/GameScreen.js
+++ TowerTopper/ClientApp/src/components/GameScreen.js
@@ -1,56 +1,25 @@
 ﻿import React, { Component } from 'react';
-var Background = require('../assets/background.png');
+import ImageLoader from './ImageLoader';
+import Background from '../assets/background.png';
 
 class GameScreen extends Component {
+    imagesToLoad = {
+        background: Background
+    };
+
     constructor(props) {
         super(props);
         this.state = { imagesLoaded: false };
-        this.loaded = false;
     }
 
-    createRoom = () => {
-        this.props.connection
-            .send('CreateRoom', this.state.userName)
-            .catch(err => console.error(err));
-    }
-
-    joinRoom = (gameId) => {
-        this.props.connection
-            .send('JoinGame', gameId)
-            .catch(err => console.error(err));
-    }
-
-    roomCreated = (roomCreatedEvent) => {
-        if (this.props.onJoin) {
-            this.props.onJoin({ userName: this.state.userName, roomCode: roomCreatedEvent.roomCode });
-        }
-    };
-
-    componentDidMount = () => {
-        this.props.connection.on('RoomCreated', this.roomCreated);
-        this.loadImages();
+    onImagesLoaded = (images) => {
+        this.images = images;
+        this.setState({ imagesLoaded: true });
         this.animationFrame = window.requestAnimationFrame(this.draw);
     }
 
-    loadImages = () => {
-        this.loadedImages = 0;
-        this.images = {
-            background: this.loadImage(Background)
-        };
-    }
-
-    loadImage = () => {
-        const img = new Image();
-        img.onload = this.imageLoaded;
-        img.src = Background;
-        return img;
-    }
-
-    imageLoaded = () => {
-        this.loadedImages++;
-        if (this.loadedImages == Object.keys(this.images).length) {
-            this.loaded = true;
-        }
+    componentDidMount = () => {
+        this.props.connection.on('RoomCreated', this.roomCreated);
     }
 
     componentWillUnmount = () => {
@@ -62,19 +31,21 @@ class GameScreen extends Component {
         var ctx = this.refs.canvas.getContext('2d');
         ctx.clearRect(0, 0, 800, 480);
         ctx.imageSmoothingEnabled = false;
-        if (this.loaded) {
-            ctx.drawImage(this.images.background, 0, 0, 800, 480);
-        } else {
-            ctx.fillText("Loading images: " + this.loadedImages + "/" + Object.keys(this.images).length, 10, 10);
-        }
+        ctx.drawImage(this.images.background, 0, 0, 800, 480);
 
         this.animationFrame = window.requestAnimationFrame(this.draw);
     }
 
     render() {
-        return (
-            <canvas ref="canvas" width={800} height={480} />
-        );
+        if (this.state.imagesLoaded) {
+            return (
+                <canvas ref="canvas" width={800} height={480} />
+            );
+        } else {
+            return (
+                <ImageLoader images={this.imagesToLoad} onLoad={this.onImagesLoaded} />
+            );
+        }
     }
 }
 
---
+++ TowerTopper/ClientApp/src/components/ImageLoader.js
@@ -0,0 +1,45 @@
+﻿import React, { Component } from 'react';
+var Background = require('../assets/background.png');
+
+class ImageLoader extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { imagesLoaded: 0 };
+    }
+
+    componentDidMount = () => {
+        this.loadImages();
+    }
+
+    loadImages = () => {
+        this.images = {};
+        for (const property in this.props.images) {
+            this.images[property] = this.loadImage(this.props.images[property]);
+        };
+        console.log(this.images);
+    }
+
+    loadImage = () => {
+        const img = new Image();
+        img.onload = this.imageLoaded;
+        img.src = Background;
+        return img;
+    }
+
+    imageLoaded = () => {
+        console.log(this.state.imagesLoaded);
+        const imagesLoaded = this.state.imagesLoaded + 1;
+        this.setState({ imagesLoaded: imagesLoaded });
+        if (imagesLoaded === Object.keys(this.props.images).length) {
+            this.props.onLoad(this.images);
+        }
+    }
+
+    render() {
+        return (
+            <span>Loading... {Math.round(this.state.imagesLoaded / Object.keys(this.props.images).length * 100)}%</span>
+        );
+    }
+}
+
+export default ImageLoader;
