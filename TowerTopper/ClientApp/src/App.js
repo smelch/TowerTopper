e473137c640d8635ab22ac4fe46d34cd8ead0380
Eric Blatz <blatz.eric@gmail.com>
2020-08-28 00:55:26 -0700

    Removing the chrome

--- TowerTopper/ClientApp/src/App.js
+++ TowerTopper/ClientApp/src/App.js
@@ -12,13 +12,8 @@ export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/games' component={TowerTopper} />
-      </Layout>
+      return (
+        <TowerTopper />
     );
   }
 }
